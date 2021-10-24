b65a45210e1a4d6b43d1a4f5e53e3910aa626b6b
julietamorrazo <92743415+julietamorrazo@users.noreply.github.com>
2021-10-24 09:37:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+var persona = {
+    nombre: "Juana",
+    apellido: "Herrera",
+    edad: 25,  
+    direccion: {
+        pais: "Costa Rica",
+        ciudad: "San Jose",
+            }
+
+}
+
+console.log (persona.direccion.pais)
